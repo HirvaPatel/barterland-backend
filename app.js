4c47002e304c4f7f32238b47ca06a8827f646d14
Hirva Patel <hirva.patel@dal.ca>
2022-03-25 09:15:39 -0300

    Added get myads route

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 const express = require("express");
 const req = require("express/lib/request");
 const res = require("express/lib/response");
+const myAdsRoute = require("./routes/getMyAdsRoute");
 
 const app = express();
 
 app.use(express.json());
 
-var mongo = require('./mongo');
-
+var mongo = require("./mongo");
 
 mongo.connectDB(async (err) => {
-    if (err) throw err;
+  if (err) throw err;
 
-    // Mongo test implementation
-    const db = mongo.getDatabase();
-    const usersCollection = db.collection('users').find().toArray()
-        .then(results => {
-            console.log(results)
-        })
-        .catch(error => console.error(error));
+  // Mongo test implementation
+  const db = mongo.getDatabase();
+  const usersCollection = db
+    .collection("users")
+    .find()
+    .toArray()
+    .then((results) => {
+      console.log(results);
+    })
+    .catch((error) => console.error(error));
 
-
-
-    app.use(function (req, res) {
-        res.status(404);
-        const response = {
-            message: "No mapping found for the requested resource - " + req.originalUrl, success: false
-        }
-        res.json(response);
-        return;
-    });
-
+  app.use(function (req, res) {
+    res.status(404);
+    const response = {
+      message:
+        "No mapping found for the requested resource - " + req.originalUrl,
+      success: false,
+    };
+    res.json(response);
+    return;
+  });
 });
 
+app.get("/", (req, res, next) => {
+  res.send("Welcome to the Barterland Backend");
+});
+app.use("/myads", myAdsRoute);
 module.exports = app;
