83a5ee25e10dcc7788fbc168ba3ef26cdcd65101
Vikram Babu Rajendran <vk712598@dal.ca>
2022-03-26 02:17:57 -0300

    API's for Home & Deals

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 const express = require("express");
+const cors = require('cors');
+
 const req = require("express/lib/request");
 const res = require("express/lib/response");
 
+const homeRouter = require("./home/homeRouter");
+const dealsRouter = require("./deals/dealsRouter");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 var mongo = require('./mongo');
 
 
-mongo.connectDB(async (err) => {
-    if (err) throw err;
+// mongo.connectDB(async (err) => {
+//     if (err) throw err;
 
-    // Mongo test implementation
-    const db = mongo.getDatabase();
-    const usersCollection = db.collection('users').find().toArray()
-        .then(results => {
-            console.log(results)
-        })
-        .catch(error => console.error(error));
+//     // Mongo test implementation
+//     const db = mongo.getDatabase();
+//     const usersCollection = db.collection('advertisments').find().toArray()
+//         .then(results => {
+//             console.log(results)
+//         })
+//         .catch(error => console.error(error));
 
+    
 
+    app.use("/home", homeRouter);
+    app.use("/deals",dealsRouter);
 
     app.use(function (req, res) {
         res.status(404);
@@ -31,6 +39,6 @@ mongo.connectDB(async (err) => {
         return;
     });
 
-});
+// });
 
 module.exports = app;
