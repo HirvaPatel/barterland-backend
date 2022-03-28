07f5211492d17e5662630b95bcb0fd5fe5538088
Hirva Patel <hirva.patel@dal.ca>
2022-03-28 19:06:37 -0300

    Added user session management

--- app.js
+++ app.js
@@ -1,48 +1,57 @@
 const express = require("express");
+const cors = require("cors");
+
 const req = require("express/lib/request");
 const res = require("express/lib/response");
-const myAdsRoute = require("./routes/getMyAdsRoute");
-const updateMyAdRoute = require("./routes/updateMyAdRoute");
-const deleteMyRoute = require("./routes/deleteMyAdRoute");
-var cors = require("cors");
+const mongoose = require("mongoose");
+
+const homeRouter = require("./home/homeRouter");
+const dealsRouter = require("./deals/dealsRouter");
+const myAdsRoute = require("./myAds/getMyAdsRoute");
+const updateMyAdRoute = require("./myAds/updateMyAdRoute");
+const deleteMyRoute = require("./myAds/deleteMyAdRoute");
+const rootRoute = "/api";
 
 const app = express();
 
-app.use(cors({ origin: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
 
 var mongo = require("./mongo");
 
-mongo.connectDB(async (err) => {
-  if (err) throw err;
+const wishlistRoute = require("./Wishlist/routes/wishlistRoute");
 
-  // Mongo test implementation
-  const db = mongo.getDatabase();
-  const usersCollection = db
-    .collection("users")
-    .find()
-    .toArray()
-    .then((results) => {
-      console.log(results);
-    })
-    .catch((error) => console.error(error));
+const UserRoute = require("./api/routes/users");
+app.use(rootRoute, UserRoute);
 
-  app.use(function (req, res) {
-    res.status(404);
-    const response = {
-      message:
-        "No mapping found for the requested resource - " + req.originalUrl,
-      success: false,
-    };
-    res.json(response);
-    return;
-  });
-});
-
-app.get("/", (req, res, next) => {
-  res.send("Welcome to the Barterland Backend");
-});
+app.use("/home", homeRouter);
+app.use("/deals", dealsRouter);
 app.use("/myads", myAdsRoute);
 app.use("/updatemyad", updateMyAdRoute);
 app.use("/deletemyad", deleteMyRoute);
+app.use("/wishlist", wishlistRoute);
+
+app.use(function (req, res) {
+  res.status(404);
+  const response = {
+    message: "No mapping found for the requested resource - " + req.originalUrl,
+    success: false,
+  };
+  res.json(response);
+  return;
+});
+
+app.use("*", (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+  res.setHeader("Access-Control-Max-Age", "3600");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Accept, X-Requested-With, Authorization"
+  );
+  next();
+});
+
 module.exports = app;
