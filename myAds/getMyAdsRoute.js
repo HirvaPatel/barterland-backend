b3ac5b3a930801c8c64b067863426602622ed4e7
Hirva Patel <hirva.patel@dal.ca>
2022-03-29 21:36:29 -0300

    Added comments to myAds

--- myAds/getMyAdsRoute.js
+++ myAds/getMyAdsRoute.js
@@ -5,16 +5,18 @@ var mongo = require("../mongo");
 const express = require("express");
 const app = express();
 
+//use json body for the route
 app.use(express.json());
 
 const router = express.Router();
 
+//get endpoint to fetch the advertisements
 router.get("", (req, res) => {
   const user_id = req.headers.user_id;
   mongo.connectDB(async (err) => {
     if (err) throw err;
 
-    // Mongo test implementation
+    //connect to mongo database
     const db = mongo.getDatabase();
     const adsCollection = db
       .collection("advertisments")
@@ -28,6 +30,7 @@ router.get("", (req, res) => {
       })
       .catch((error) => console.error(error));
 
+    //handle the not found error for the api
     app.use(function (req, res) {
       res.status(404);
       const response = {
--- myAds/updateMyAdRoute.js
+++ myAds/updateMyAdRoute.js
@@ -5,13 +5,17 @@ var mongo = require("../mongo");
 const express = require("express");
 const app = express();
 
+//use json body for the route
 app.use(express.json());
 const router = express.Router();
 
+//put endpoint to update the advertisement
 router.put("", (req, res) => {
   const filter = { ad_id: req.body.ad_id };
   // this option instructs the method to create a document if no documents match the filter
   const updateOptions = { upsert: true };
+
+  //fetch the advertisement from the request body
   const updateDoc = {
     $set: {
       "ad_details.description": req.body.description,
@@ -24,7 +28,7 @@ router.put("", (req, res) => {
   mongo.connectDB(async (err) => {
     if (err) throw err;
 
-    // Mongo test implementation
+    //connect to the mongo database
     const db = mongo.getDatabase();
     const adsCollection = db
       .collection("advertisments")
@@ -44,6 +48,7 @@ router.put("", (req, res) => {
       })
       .catch((error) => console.error(error));
 
+    //handle the not found error for the api
     app.use(function (req, res) {
       res.status(404);
       const response = {
