8b920c63844a632941cc6b1ad84749e05a38d9fd
sw891064@dal.ca <sw891064@dal.ca>
2022-04-04 02:22:19 -0300

    Search feature backend

--- search/searchRouter.js
+++ search/searchRouter.js
@@ -12,16 +12,46 @@ mongo.connectDB(async (err) => {
     if (err) throw err;
     const db = mongo.getDatabase();
 
-    router.get('/getad/:value', async (req,res) => {
-            const value = req.params.value;
-            console.log(value);
-            
-          
-                    
+    const collection = db.collection("advertisments")
+
+    router.get('/getad/:value', async (req, res) => {
+        const value = req.params.value;
+
+        let filtervalue = new RegExp(["^", value, "$"].join(""), "i");
+        console.log(filtervalue);
+
+
+        console.log(value);
+        collection.find({ $text: { $search: value } }).toArray().then((results) => {
+            console.log(results.length);
+             
+            if (results.length > 0) {
+                const response = {
+                    success: true,
+                    data: results,
+                };
+                return res.status(200).json(response);
+            }
+            const response = {
+                success: false,
+                data: null,
+                message: "No product found!"
+            };
+            return res.status(400).json(response);
+        })
+            .catch((error) => {
+                console.error(error);
+                const response = {
+                    success: false,
+                    data: null,
+                    message: "No product found!"
+                };
+                return res.status(400).json(response);
+            });
 
     });
-    
-   
+
+
 });
 
 module.exports = router;
